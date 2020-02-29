bce068300b54b4db93a90854a8cb3b9abf519e75
AmirH Ahmadi <amirh.khali@gmail.com>
2020-02-29 12:35:13 +0330

    A3 first commit

---
+++ A3/A3/Q2DetectingAnomalies.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+namespace A3
+{
+    public class Q2DetectingAnomalies:Processor
+    {
+        public Q2DetectingAnomalies(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long>)Solve);
+
+        public long[] dist;
+
+        public long Solve(long nodeCount, long[][] edges)
+        {
+            for (int i = 0; i < edges.Length; ++i) {
+                edges[i][0]--;
+                edges[i][1]--;
+            }
+            long[] dist = new long[nodeCount];
+            for (int i = 0; i < nodeCount; ++i) {
+                dist[i] = int.MaxValue;
+            }
+            for (int k = 0; k < nodeCount; ++k) {
+                if (dist[k] != int.MaxValue) continue;
+                dist[k] = 0;
+                for (int i = 0; i < nodeCount - 1; ++i) {
+                    for (int j = 0; j < edges.Length; ++j) {
+                        if (dist[edges[j][1]] > dist[edges[j][0]] + edges[j][2]) {
+                            dist[edges[j][1]] = dist[edges[j][0]] + edges[j][2];
+                        }
+                    }
+                }
+
+                for (int j = 0; j < edges.Length; ++j) {
+                    if (dist[edges[j][1]] > dist[edges[j][0]] + edges[j][2]) {
+                        return 1;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ A3/A3/Q4FriendSuggestion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A3
+{
+    public class Q4FriendSuggestion:Processor
+    {
+        public Q4FriendSuggestion(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long[][], long,long[][], long[]>)Solve);
+
+        public long[] Solve(long nodeCount, long edgeCount, 
+                              long[][] edges, long queriesCount, 
+                              long[][]queries) {
+            
+            long[] ans = new long[queries.Length];
+            long[,] dist = new long[nodeCount, nodeCount];
+            for (int i = 0; i < nodeCount; ++i) {
+                for (int j = 0; j < nodeCount; ++j) {
+                    dist[i, j] = int.MaxValue;
+                    if (i == j) dist[i, j] = 0;
+                }
+            }
+
+            for (int i = 0; i < edges.Length; ++i) {
+                edges[i][0]--;
+                edges[i][1]--;
+                dist[edges[i][0], edges[i][1]] = edges[i][2];
+            }
+
+            for (int i = 0; i < nodeCount; ++i) {
+                for (int j = 0; j < nodeCount; ++j) {
+                    for (int k = 0; k < nodeCount; ++k) {
+                        if (dist[j, k] > dist[j, i] + dist[i, k]) {
+                            dist[j, k] = dist[j, i] + dist[i, k];
+                        }
+                    }
+                }
+            }
+            
+            for (int i = 0; i < queries.Length; ++i) {
+                queries[i][0]--;
+                queries[i][1]--;
+                ans[i] = dist[queries[i][0], queries[i][1]];
+            }
+            return ans;
+        }
+    }
+}

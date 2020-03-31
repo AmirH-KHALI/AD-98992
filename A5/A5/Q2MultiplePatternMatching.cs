b8ba0ca19710ed929dbc2e68d0eaf7b60dd8dcdb
AmirH Ahmadi <amirh.khali@gmail.com>
2020-03-31 17:14:46 +0430

    A5 1,2,3,4

---
+++ A5/A5/Q2MultiplePatternMatching.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A5
+{
+    public class Q2MultiplePatternMatching : Processor
+    {
+        public Q2MultiplePatternMatching(string testDataName) : base(testDataName)
+        {
+            //ExcludeTestCaseRangeInclusive(1, 1);
+            //ExcludeTestCaseRangeInclusive(3, 50);
+        }
+
+        public override string Process(string inStr) =>
+        TestTools.Process(inStr, (Func<String, long, String[], long[]>)Solve);
+
+        public long[] Solve(string text, long n, string[] patterns)
+        {
+            List<List<long>> adj = new List<List<long>>();
+            List<String> w = new List<String>();
+
+            adj.Add(new List<long>());
+            w.Add(null);
+
+            for (int i = 0; i < n; ++i) {
+                build(adj, w, patterns[i] + "#");
+            }
+
+            List<long> ans = new List<long>();
+            find(text + "#", adj, w, ans);
+            if (ans.Count == 0) ans.Add(-1);
+            return ans.ToArray();
+            /*List<long> ans = new List<long>();
+            for (int i = 0; i < text.Length; ++i) {
+                bool find = false;
+                for (int j = 0; j < patterns.Length; ++j) {
+                    find = true;
+                    for (int k = 0; k < patterns[j].Length; ++k) {
+                        if (i + k >= text.Length || text[i + k] != patterns[j][k]) {
+                            find = false;
+                        }
+                    }
+                    if (find) break;
+                }
+                if (find) ans.Add(i);
+            }
+            if (ans.Count == 0) ans.Add(-1);
+            return ans.ToArray();*/
+        }
+
+        private void find(string text, List<List<long>> adj, List<string> w, List<long> ans) {
+            for (int i = 0; i < text.Length; ++i) {
+                int currentNode = 0;
+                bool flag = false;
+                for (int j = i; j < text.Length; ++j) {
+                    int find = 0;
+                    for (int k = 0; k < adj[currentNode].Count; ++k) {
+                        int child = (int)adj[currentNode][k];
+                        if (w[child].Equals("#")) {
+                            flag = true;
+                            break;
+                        }
+                        if (w[child].Equals(text[j].ToString())) {
+                            find = child;
+                        }
+                    }
+                    if (flag || (find == 0)) break;
+                    currentNode = find;
+                }
+                if (flag) {
+                    ans.Add(i);
+                }
+            }
+        }
+
+        private void build(List<List<long>> adj,
+                         List<string> w,
+                         string pattern) {
+
+            int currentNode = 0;
+            for (int i = 0; i < pattern.Length; ++i) {
+                bool flag = false;
+                for (int j = 0; j < adj[currentNode].Count; ++j) {
+                    int child = (int)adj[currentNode][j];
+                    if (w[child] == pattern[i].ToString()) {
+                        currentNode = child;
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) {
+                    adj.Add(new List<long>());
+                    w.Add(pattern[i].ToString());
+                    adj[currentNode].Add(adj.Count - 1);
+                    currentNode = adj.Count - 1;
+                }
+            }
+        }
+    }
+}
---
+++ A5/A5/Q4SuffixTree.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A5
+{
+    public class Q4SuffixTree : Processor {
+        public Q4SuffixTree(string testDataName) : base(testDataName) {
+            //ExcludeTestCaseRangeInclusive(1, 33);
+            //ExcludeTestCaseRangeInclusive(5, 50);
+            this.VerifyResultWithoutOrder = true;
+        }
+
+        public override string Process(string inStr) =>
+        TestTools.Process(inStr, (Func<String, String[]>)Solve);
+
+
+
+        public string[] Solve(string text) {
+
+            SuffixTree suffixTree = new SuffixTree(text);
+
+            List<string> ans = new List<string>();
+            
+            suffixTree.dfs(0, ans);
+
+            return ans.ToArray();
+        }
+
+        public class Node {
+            public string value;
+            public List<int> children; 
+
+            public Node() {
+                value = "";
+                children = new List<int>();
+            }
+
+            public Node(string value, params int[] children) {
+                this.value = value;
+                this.children = new List<int>();
+                this.children.AddRange(children);
+            }
+        }
+
+        public class SuffixTree {
+
+            public List<Node> nodes;
+
+            public SuffixTree(string text) {
+                nodes = new List<Node>();
+                nodes.Add(new Node());
+
+                for (int i = 0; i < text.Length; ++i) {
+                    addSuffix(text.Substring(i));
+                }
+
+                mark = new bool[nodes.Count];
+            }
+
+            bool[] mark;
+            public void dfs(int x, List<string> ans) {
+                mark[x] = true;
+                for (int i = 0; i < nodes[x].children.Count; ++i) {
+                    int child = nodes[x].children[i];
+                    if (!mark[child]) {
+                        ans.Add(nodes[child].value);
+                        dfs(child, ans);
+                    }
+                }
+            }
+
+            private void addSuffix(string str) {
+                int currentNode = 0;
+                int i = 0;
+                while (i < str.Length) {
+                    List<int> children = nodes[currentNode].children;
+                    int pChildIndex = -1;
+                    for (int j = 0; j < children.Count; ++j) {
+                        int child = children[j];
+                        if (nodes[child].value[0] == str[i]) {
+                            pChildIndex = j;
+                            break;
+                        }
+                    }
+                    if (pChildIndex == -1) {
+                        nodes.Add(new Node(str.Substring(i)));
+                        children.Add(nodes.Count - 1);
+                        return;
+                    }
+
+                    int pNode = children[pChildIndex];
+                    string pEdgeVal = nodes[pNode].value;
+                    int k;
+                    for (k = 0; k < pEdgeVal.Length; ++k) {
+                        if (str[i + k] != pEdgeVal[k]) {
+                            nodes.Add(new Node(pEdgeVal.Substring(0, k), pNode));
+                            nodes[pNode].value = pEdgeVal.Substring(k);
+                            pNode = children[pChildIndex] = nodes.Count - 1;
+                            break;
+                        }
+                    }
+                    currentNode = pNode;
+                    i += k;
+                }
+            }
+
+        }
+    }
+}

40b23183d51fcfc2d670788bc24643aa258e122a
AmirH Ahmadi <amirh.khali@gmail.com>
2020-05-19 00:53:17 +0430

    A9, Q1

---
+++ A9/A9/Q1InferEnergyValues.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TestCommon;
+
+namespace A9
+{
+    public class Q1InferEnergyValues : Processor
+    {
+        public Q1InferEnergyValues(string testDataName) : base(testDataName)
+        {
+            //ExcludeTestCaseRangeInclusive(1, 27);
+        }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, double[,], double[]>)Solve);
+
+        public double[] Solve(long n, double[,] matrix)
+        {
+            bool[] mark = new bool[matrix.GetLength(0)]; //lodum satr ha 1 shodan
+
+            long[] order = new long[matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(1) - 1; ++i) { // sefr kardan e sotun i om
+                for (int j = 0; j < matrix.GetLength(0); ++j) { // peyda kardan satr qeir e sefr
+                    if (!mark[j] && matrix[j, i] != 0) {
+                        mark[j] = true;
+                        order[i] = j;
+                        double temp = matrix[j, i];
+                        for (int k = i; k < matrix.GetLength(1); ++k) { 
+                            matrix[j, k] /= temp;
+                        }
+                        for (int k = 0; k < matrix.GetLength(0); ++k) {
+                            if (k == j) continue;
+                            double mazrab = matrix[k, i] / matrix[j, i];
+                            for (int l = i; l < matrix.GetLength(1); ++l) {
+                                matrix[k, l] -= matrix[j, l] * mazrab;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+
+            double[] ans = new double[matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); ++i) {
+                double t = matrix[order[i], matrix.GetLength(1) - 1];
+                double abst = Math.Abs(t);
+                if (abst - ((int)abst) < .25) abst = (int)abst;
+                else if (((int)abst + 1) - abst < .25) abst = (int)abst + 1;
+                else abst = (int)abst + .5;
+                if (Math.Abs(t) == 0) ans[i] = 0;
+                else ans[i] = (t / Math.Abs(t)) * abst;
+                if (ans[i] == -0) ans[i] = 0;
+            }
+            return ans;
+        }
+    }
+}
---
+++ A9/A9/Q3OnlineAdAllocation.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+namespace A9
+{
+    public class Q3OnlineAdAllocation : Processor
+    {
+
+        public Q3OnlineAdAllocation(string testDataName) : base(testDataName)
+        {
+        }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<int, int, double[,], String>)Solve);
+
+        public string Solve(int c, int v, double[,] matrix1)
+        {
+            List<Equation> equations = new List<Equation>();
+            for (int i = 0; i < c; ++i) {
+                double[] temp = new double[v + 1];
+                for (int j = 0; j < v; ++j) {
+                    temp[j] = matrix1[i, j];
+                }
+
+                temp[v] = matrix1[i, v];
+                equations.Add(new Equation(temp));
+            }
+
+            return "";
+
+            //makeTable(c, v, equations, matrix1);
+
+        }
+
+
+        //private void makeTable(int c, int v, List<Equation> equations, double[,] matrix1) {
+        //    List<double[]> table = new List<double[]>();
+        //    int S = 0;
+        //    int A = 0;
+
+        //    for (int i = 0; i < c; ++i) {
+        //        if (equations[i].constant >= 0) {
+        //            equations[i].zarib.Add(1);
+        //            S++;
+        //        }
+
+        //        if (equations[i].constant < 0) {
+        //            equations[i].isNeg = true;
+        //            for (int j = 0; j < v; j++) {
+        //                equations[i].zarib[j] *= -1;
+        //            }
+
+        //            equations[i].constant *= -1;
+
+        //            equations[i].zarib.Add(-1);
+        //            equations[i].zarib.Add(1);
+        //            S++;
+        //            A++;
+        //        }
+        //    }
+
+        //    NewVarCount = VarCount + S + A;
+        //    int x = 0;
+        //    for (int i = 0; i < EquCount; i++) {
+        //        double[] tmp = new double[NewVarCount + 1];
+        //        for (int j = 0; j < VarCount; j++) {
+        //            tmp[j] = Equations[i].Variables[j];
+        //        }
+        //        tmp[VarCount + i] = Equations[i].Variables[VarCount];
+
+        //        if (Equations[i].HasA) {
+        //            tmp[VarCount + S + x++] = Equations[i].Variables[VarCount + 1];
+        //        }
+        //        tmp[NewVarCount] = Equations[i].constant;
+
+        //        table.Add(tmp);
+        //    }
+        //    double[] t = new double[NewVarCount + 1];
+        //    for (int i = 0; i < VarCount; i++) {
+        //        t[i] = Matrix1[EquCount, i] * -1;
+        //    }
+        //    table.Add(t);
+
+        //    this.Table = table.ToArray();
+
+        //    int s = 0;
+        //    int a = 0;
+        //    for (int i = 0; i < EquCount; i++) {
+        //        if (Equations[i].HasA) {
+        //            FirstColumn[i] = "A" + a.ToString();
+        //            a++;
+        //            s++;
+        //        } else {
+        //            FirstColumn[i] = "S" + s.ToString();
+        //            s++;
+        //        }
+        //    }
+
+        //    FirstRow = new string[NewVarCount];
+        //    int xx = 0;
+        //    s = 0;
+        //    a = 0;
+        //    for (int i = 0; i < VarCount; i++)
+        //        FirstRow[i] = "X" + xx++.ToString();
+
+        //    for (int i = VarCount; i < VarCount + S; i++)
+        //        FirstRow[i] = "S" + s++.ToString();
+
+        //    for (int i = VarCount + S; i < VarCount + S + A; i++)
+        //        FirstRow[i] = "A" + a++.ToString();
+        //}
+
+    public class Equation {
+
+            public List<double> zarib;//:)
+            public double constant;
+            public bool isNeg;
+
+            public Equation(double[] zarib) {
+                this.zarib = new List<double>();
+                for (int i = 0; i < zarib.Length - 1; ++i) {
+                    this.zarib.Add(zarib[i]);
+                }
+                this.constant = zarib[zarib.Length - 1];
+            }
+        }
+    }
+}

5c7faf57919a45d90683c0a3382d4ceb72148d57
AmirH Ahmadi <amirh.khali@gmail.com>
2020-07-07 08:53:26 +0430

    E2 first commit

---
+++ E2/E2/Q1MaxflowVertexCapacity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TestCommon;
+
+namespace E2
+{
+    public class Q1MaxflowVertexCapacity : Processor
+    {
+        public Q1MaxflowVertexCapacity(string testDataName) : base(testDataName)
+        {}
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long[][],long[] , long, long, long>)Solve);
+
+        public virtual long Solve(long nodeCount, 
+            long edgeCount, long[][] edges, long[] nodeWeight, 
+            long startNode , long endNode)
+        {
+            // write your code here
+            throw new NotImplementedException();  
+        }
+    }
+}
---
+++ E2/E2/Q2BoardGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TestCommon;
+
+namespace E2
+{
+    public class Q2BoardGame : Processor
+    {
+        public Q2BoardGame(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<int, long[,], string[]>)Solve);
+
+        public string[] Solve(int BoardSize, long[,] Board)
+        {
+            // write your code here
+            throw new NotImplementedException();            
+        }
+        public override Action<string, string> Verifier { get; set; } =
+            TestTools.SatVerifier;
+    }
+}
